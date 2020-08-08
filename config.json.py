05f170c73e9034dcf6f895c0d1e7599f95504eed
DhyanTD <dhyantd@gmail.com>
2020-08-08 21:13:14 +0530

    Initial commit

---
+++ config.json.py
@@ -0,0 +1,23 @@
+{
+  "params":
+    {
+
+        "local_server":"True",
+        "local_uri": "mysql://root:@localhost/codewithdhyan",
+        "prod_uri":"mysql://root:@localhost/codewithdhyan",
+        "fb_url":"https://facebook.com/codewithdhyan",
+        "tw_url":"https://twitter.com/codewithdhyan",
+        "gh_url": "https://github.com/codewithdhyan",
+        "blog_name":"codewithdhyan",
+        "tittle_name":"madewithlove",
+        "sub_heading":"First Custom Made Website",
+        "gmail-user":"dhyant65@gmail.com",
+        "gmail-password":"Dhyantd1203h",
+        "about_text":"hi bruh i'll upload something soon",
+        "no_of_posts": 3,
+        "login_image":"login.svg",
+        "admin_user":"Dhyan",
+        "admin_password":"Dhyantdthegreat",
+        "upload_location":"C:\\Users\\nayana\\PycharmProjects\\flask\\static"
+    }
+}
